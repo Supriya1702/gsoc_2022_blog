86a4dfdec498efcebf89cf7842b0640d537f8c83
Supriya1702 <supriyakvmari@gmail.com>
2022-07-11 20:25:58 +0530

    updated tab name

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,8 @@ module.exports = {
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
     siteTitle: `Google Summer Of Code 2022 Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+    siteTitleAlt: `Blog | Google Summer Of Code 2022`,
+    siteHeadline: `Blog | Google Summer Of Code 2022`,
     siteUrl: `https://minimal-blog.lekoarts.de`,
     siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     siteLanguage: `en`,
